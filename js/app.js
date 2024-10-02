72d12a599296a9772b329c68057416c81057bd95
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2024-10-01 22:52:31 -0600

    finished css for desktop and most of click events for js

---
+++ js/app.js
@@ -0,0 +1,38 @@
+const plus = document.querySelectorAll(".plus");
+const minus = document.querySelectorAll(".minus");
+const answer1 = document.getElementById("answer1");
+const answers = document.querySelectorAll(".answer");
+
+plus.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("plus")) {
+      item.classList.remove("plus");
+      item.classList.add("minus");
+      item.nextElementSibling.style.display = "block";
+
+      answers.forEach((answer) => {
+        if (answer.previousElementSibling.contains(item)) {
+          answer.style.display = "block";
+        } else {
+          answer.style.display = "none";
+        }
+      });
+    }
+  });
+});
+
+minus.forEach((item) => {
+  item.addEventListener("click", () => {
+    if (item.classList.contains("minus")) {
+      item.previousElementSibling.classList.remove("minus");
+      item.previousElementSibling.classList.add("plus");
+      item.style.display = "none";
+
+      answers.forEach((answer) => {
+        if (answer.previousElementSibling.contains(item)) {
+          answer.style.display = "none";
+        }
+      });
+    }
+  });
+});

a5e7b7e5606344912e38bcd0fbc571801d62d3d5
Ramon C <68085268+Ramon-Carrillo@users.noreply.github.com>
2024-10-10 20:23:45 +0000

    need to change js code the adding and removel of padding and margin for mob.

--- js/app.js
+++ js/app.js
@@ -1,36 +1,35 @@
-const plus = document.querySelectorAll(".plus");
-const minus = document.querySelectorAll(".minus");
-const answers = document.querySelectorAll(".answer");
-const questions = document.querySelectorAll(".question");
+const plus = document.querySelectorAll('.plus')
+const minus = document.querySelectorAll('.minus')
+const answers = document.querySelectorAll('.answer')
+const questions = document.querySelectorAll('.question')
 
 const openClosePlus = plus.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (item.classList.contains("plus")) {
-      item.classList.remove("plus");
-      item.classList.add("minus");
-      item.nextElementSibling.style.display = "block";
+  item.addEventListener('click', () => {
+    if (item.classList.contains('plus')) {
+      item.classList.remove('plus')
+      item.classList.add('minus')
+      item.nextElementSibling.style.display = 'block'
 
       answers.forEach((answer) => {
         if (answer.previousElementSibling.contains(item)) {
-          answer.style.display = "block";
+          answer.style.display = 'block'
         }
-      });
+      })
     }
-  });
-});
+  })
+})
 
 const openCloseMinus = minus.forEach((item) => {
-  item.addEventListener("click", () => {
-    if (item.classList.contains("minus")) {
-      item.previousElementSibling.classList.remove("minus");
-      item.previousElementSibling.classList.add("plus");
-      item.style.display = "none";
-
+  item.addEventListener('click', () => {
+    if (item.classList.contains('minus')) {
+      item.previousElementSibling.classList.remove('minus')
+      item.previousElementSibling.classList.add('plus')
+      item.style.display = 'none'
       answers.forEach((answer) => {
         if (answer.previousElementSibling.contains(item)) {
-          answer.style.display = "none";
+          answer.style.display = 'none'
         }
-      });
+      })
     }
-  });
-});
+  })
+})
